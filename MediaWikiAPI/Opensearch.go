d8d1578ae1d28810416d346d6653636b996fe7b8
NyanCatda <nyancatda@outlook.com>
2021-10-12 16:14:44 +0800

    封装OpenSearch API

---
+++ MediaWikiAPI/Opensearch.go
@@ -0,0 +1,20 @@
+package MediaWikiAPI
+
+import (
+	"encoding/json"
+
+	"xyz.nyan/MediaWiki-Bot/utils"
+)
+
+//使用开放搜索协议搜索wiki(最多返回10条)
+//WikiName 需要查询的Wiki名字
+//title 需要搜索的页面标题
+func Opensearch(WikiName string, title string) (map[string]interface{}, error) {
+	WikiLink := GetWikiLink(WikiName)
+	url := WikiLink + "/api.php?action=opensearch&limit=10&redirects=resolve&search=" + title
+	body, err := utils.HttpRequest(url)
+
+	info := make(map[string]interface{})
+	json.Unmarshal([]byte(body), &info)
+	return info, err
+}
