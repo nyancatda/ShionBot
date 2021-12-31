153e3a3c31fb72469c15501651dbd93c6bb461fa
NyanCatda <nyancatda@outlook.com>
2021-12-31 11:25:54 +0800

    重写配置文件结构体

--- src/utils/ReadConfig.go
+++ src/utils/ReadConfig.go
@@ -1,48 +1,49 @@
+/*
+ * @Author: NyanCatda
+ * @Date: 2021-10-03 04:14:10
+ * @LastEditTime: 2021-12-31 11:24:45
+ * @LastEditors: NyanCatda
+ * @Description: 读取配置文件
+ * @FilePath: \ShionBot\src\utils\ReadConfig.go
+ */
 package utils
 
 import (
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Run  *Run        `yaml:"Run"`
-	SNS  *SNS        `yaml:"SNS"`
+	Run struct {
+		WebHookPort string `yaml:"WebHookPort"`
+		WebHookKey  string `yaml:"WebHookKey"`
+		Language    string `yaml:"Language"`
+	} `yaml:"Run"`
+	SNS struct {
+		QQ struct {
+			Switch      bool   `yaml:"Switch"`
+			APILink     string `yaml:"APILink"`
+			BotQQNumber int    `yaml:"BotQQNumber"`
+			VerifyKey   string `yaml:"VerifyKey"`
+		} `yaml:"QQ"`
+		Telegram struct {
+			Switch     bool   `yaml:"Switch"`
+			Token      string `yaml:"Token"`
+			BotAPILink string `yaml:"BotAPILink"`
+		} `yaml:"Telegram"`
+		Line struct {
+			Switch             bool   `yaml:"Switch"`
+			ChannelAccessToken string `yaml:"ChannelAccessToken"`
+			BotAPILink         string `yaml:"BotAPILink"`
+		} `yaml:"Line"`
+		KaiHeiLa struct {
+			Switch bool   `yaml:"Switch"`
+			Token  string `yaml:"Token"`
+		} `yaml:"KaiHeiLa"`
+	} `yaml:"SNS"`
 	Wiki interface{} `yaml:"Wiki"`
-}
-
-type Run struct {
-	WebHookPort string `yaml:"WebHookPort"`
-	WebHookKey  string `yaml:"WebHookKey"`
-	Language    string `yaml:"Language"`
-}
-
-type SNS struct {
-	QQ       *QQ       `yaml:"QQ"`
-	Telegram *Telegram `yaml:"Telegram"`
-	Line     *Line     `yaml:"Line"`
-	KaiHeiLa *KaiHeiLa `yaml:"KaiHeiLa"`
-}
-type QQ struct {
-	Switch      bool   `yaml:"Switch"`
-	APILink     string `yaml:"APILink"`
-	BotQQNumber int    `yaml:"BotQQNumber"`
-	VerifyKey   string `yaml:"VerifyKey"`
-}
-type Telegram struct {
-	Switch     bool   `yaml:"Switch"`
-	Token      string `yaml:"Token"`
-	BotAPILink string `yaml:"BotAPILink"`
-}
-type Line struct {
-	Switch             bool   `yaml:"Switch"`
-	ChannelAccessToken string `yaml:"ChannelAccessToken"`
-	BotAPILink         string `yaml:"BotAPILink"`
-}
-type KaiHeiLa struct {
-	Switch bool   `yaml:"Switch"`
-	Token  string `yaml:"Token"`
 }
 
 var (
