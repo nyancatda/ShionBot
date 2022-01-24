9a6a6c83b1946289d4c452040ef8c9fc1cf61049
NyanCatda <nyancatda@outlook.com>
2022-01-24 18:06:42 +0800

    修改Opensearch API

--- src/MediaWikiAPI/Opensearch.go
+++ src/MediaWikiAPI/Opensearch.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2021-10-12 16:10:33
- * @LastEditTime: 2022-01-24 17:49:47
+ * @LastEditTime: 2022-01-24 18:06:32
  * @LastEditors: NyanCatda
  * @Description: MediaWiki OpensearchAPI封装
  * @FilePath: \ShionBot\src\MediaWikiAPI\Opensearch.go
@@ -10,21 +10,23 @@ package MediaWikiAPI
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/nyancatda/ShionBot/src/utils"
 )
 
 /**
- * @description: 使用开放搜索协议搜索wiki(最多返回10条)，通常来说[0]为搜索标题，[1]为条目标题列表，[2]不知道，[3]为条目链接列表，对应[1]
+ * @description: 使用开放搜索协议搜索wiki，通常来说[0]为搜索标题，[1]为条目标题列表，[2]不知道，[3]为条目链接列表，对应[1]
  * @param {string} WikiLink Wiki链接
+ * @param {int} Limit 返回条数限制
  * @param {string} title 需要搜索的页面标题
  * @return {*}
  */
-func Opensearch(WikiLink string, title string) ([]string, error) {
-	url := WikiLink + "/api.php?action=opensearch&limit=10&redirects=resolve&search=" + title
+func Opensearch(WikiLink string, Limit int, title string) ([]interface{}, error) {
+	url := WikiLink + "/api.php?action=opensearch&limit=" + strconv.Itoa(Limit) + "&redirects=resolve&search=" + title
 	body, err := utils.HttpRequest(url)
 
-	var info []string
+	var info []interface{}
 	json.Unmarshal([]byte(body), &info)
 	return info, err
 }
