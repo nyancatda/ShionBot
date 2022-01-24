5bb257c4bcda2ca78524118750fdc966a8ecefa4
NyanCatda <nyancatda@outlook.com>
2022-01-24 20:21:26 +0800

    Line API重写

--- src/MessagePushAPI/SNSAPI/LineAPI/LineAPI.go
+++ src/MessagePushAPI/SNSAPI/LineAPI/LineAPI.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2021-11-05 23:42:17
- * @LastEditTime: 2022-01-24 19:47:34
+ * @LastEditTime: 2022-01-24 20:20:23
  * @LastEditors: NyanCatda
  * @Description: Line API
  * @FilePath: \ShionBot\src\MessagePushAPI\SNSAPI\LineAPI\LineAPI.go
@@ -10,62 +10,95 @@ package LineAPI
 
 import (
 	"encoding/json"
+	"net/http"
 
 	"github.com/nyancatda/ShionBot/src/MessagePushAPI/SNSAPI"
 	"github.com/nyancatda/ShionBot/src/Utils/HttpRequest"
 	"github.com/nyancatda/ShionBot/src/Utils/ReadConfig"
 )
 
-var sns_name string = "Telegram"
+var (
+	SNSName = "Line"
+)
 
-//发送推送消息
-//to 消息接收的聊天ID
-//messages 消息内容
-//notificationDisabled 是否需要静默发送
-func SendPushMessage(chat_type string, to string, messages string, notificationDisabled bool) {
+/**
+ * @description: 发送消息
+ * @param {string} chat_type 聊天类型
+ * @param {string} to 消息接收的聊天ID
+ * @param {string} messages 消息内容
+ * @param {bool} notificationDisabled 是否需要静默发送
+ * @return {[]byte}
+ * @return {*http.Response}
+ * @return {error}
+ */
+func SendPushMessage(chat_type string, to string, messages string, notificationDisabled bool) ([]byte, *http.Response, error) {
 	Config := ReadConfig.GetConfig
-	Json := map[string]interface{}{
-		"to":                   to,
-		"notificationDisabled": notificationDisabled,
-	}
-	JsonMessages := make([]map[string]string, 1)
-	JsonMessages[0] = map[string]string{
+
+	//组成消息链
+	MessageChain := make([]map[string]string, 1)
+	MessageChain[0] = map[string]string{
 		"type": "text",
 		"text": messages,
 	}
-	Json["messages"] = JsonMessages
-	JsonBody, _ := json.Marshal(Json)
+	MessagesJson := map[string]interface{}{
+		"to":                   to,
+		"notificationDisabled": notificationDisabled,
+		"messages":             MessageChain,
+	}
+
+	JsonBody, _ := json.Marshal(MessagesJson)
 	requestBody := string(JsonBody)
 
 	url := Config.SNS.Line.BotAPILink + "v2/bot/message/push"
+	//请求头添加令牌
 	Header := []string{"Authorization:Bearer " + Config.SNS.Line.ChannelAccessToken}
-	HttpRequest.PostRequestJson(url, requestBody, Header)
+	Body, HttpResponse, err := HttpRequest.PostRequestJson(url, requestBody, Header)
 
-	SNSAPI.Log(sns_name, chat_type, to, messages)
+	//没有遇到错误则写入日志
+	if err != nil {
+		SNSAPI.Log(SNSName, chat_type, to, messages)
+	}
+
+	return Body, HttpResponse, err
 }
 
-//发送回复消息
-//replyToken 消息回复令牌
-//messages 消息内容
-//notificationDisabled 是否需要静默发送
-func SendReplyMessage(chat_type string, replyToken string, messages string, notificationDisabled bool) {
+/**
+ * @description: 发送回复消息
+ * @param {string} chat_type 聊天类型
+ * @param {string} replyToken 消息回复令牌
+ * @param {string} messages 消息内容
+ * @param {bool} notificationDisabled 是否需要静默发送
+ * @return {[]byte}
+ * @return {*http.Response}
+ * @return {error}
+ */
+func SendReplyMessage(chat_type string, replyToken string, messages string, notificationDisabled bool) ([]byte, *http.Response, error) {
 	Config := ReadConfig.GetConfig
-	Json := map[string]interface{}{
-		"replyToken":           replyToken,
-		"notificationDisabled": notificationDisabled,
-	}
-	JsonMessages := make([]map[string]string, 1)
-	JsonMessages[0] = map[string]string{
+
+	//组成消息链
+	MessageChain := make([]map[string]string, 1)
+	MessageChain[0] = map[string]string{
 		"type": "text",
 		"text": messages,
 	}
-	Json["messages"] = JsonMessages
+	Json := map[string]interface{}{
+		"replyToken":           replyToken,
+		"notificationDisabled": notificationDisabled,
+		"messages":             MessageChain,
+	}
+
 	JsonBody, _ := json.Marshal(Json)
 	requestBody := string(JsonBody)
 
 	url := Config.SNS.Line.BotAPILink + "v2/bot/message/reply"
+	//请求头添加令牌
 	Header := []string{"Authorization:Bearer " + Config.SNS.Line.ChannelAccessToken}
-	HttpRequest.PostRequestJson(url, requestBody, Header)
+	Body, HttpResponse, err := HttpRequest.PostRequestJson(url, requestBody, Header)
 
-	SNSAPI.Log(sns_name, chat_type, replyToken, messages)
+	//没有遇到错误则写入日志
+	if err != nil {
+		SNSAPI.Log(SNSName, chat_type, replyToken, messages)
+	}
+
+	return Body, HttpResponse, err
 }
