b4787d035e848b0b20ea9cc3f1580e3959dbc8c5
NyanCatda <nyancatda@outlook.com>
2022-01-24 16:47:19 +0800

    重写QueryInfoUrl

--- src/MediaWikiAPI/Query.go
+++ src/MediaWikiAPI/Query.go
@@ -1,4 +1,11 @@
-//MediaWikiAPI封装
+/*
+ * @Author: NyanCatda
+ * @Date: 2021-10-03 20:50:06
+ * @LastEditTime: 2022-01-24 16:46:25
+ * @LastEditors: NyanCatda
+ * @Description: MediaWikiAPI封装
+ * @FilePath: \ShionBot\src\MediaWikiAPI\Query.go
+ */
 package MediaWikiAPI
 
 import (
@@ -10,7 +17,13 @@ import (
 	"github.com/nyancatda/ShionBot/src/utils"
 )
 
-//读取Wiki名字对应的Wiki链接
+/**
+ * @description: 读取Wiki名字对应的Wiki链接
+ * @param {string} SNSName 聊天软件名字
+ * @param {Struct.WebHookJson} Messagejson 消息Json
+ * @param {string} WikiName Wiki名字
+ * @return {*}
+ */
 func GetWikiLink(SNSName string, Messagejson Struct.WebHookJson, WikiName string) string {
 	//获取用户配置
 	db := utils.SQLLiteLink()
@@ -40,17 +53,38 @@ func GetWikiLink(SNSName string, Messagejson Struct.WebHookJson, WikiName string
 	return ""
 }
 
-//查询页面信息，返回带URL
-//SNSName 聊天软件名字
-//Messagejson 消息json
-//WikiName 需要查询的Wiki名字
-//title 需要查询的页面标题
-func QueryInfoUrl(SNSName string, Messagejson Struct.WebHookJson, WikiName string, title string) (map[string]interface{}, error) {
-	WikiLink := GetWikiLink(SNSName, Messagejson, WikiName)
+type QueryInfoUrlJson struct {
+	Batchcomplete string `json:"batchcomplete"`
+	Query         struct {
+		Pages map[string]struct {
+			PageID               int    `json:"pageid"` //页面ID
+			Ns                   int    `json:"ns"`
+			Title                string `json:"title"`                //页面标题
+			Contentmodel         string `json:"contentmodel"`         //页面类型
+			Pagelanguage         string `json:"pagelanguage"`         //页面语言
+			Pagelanguagehtmlcode string `json:"pagelanguagehtmlcode"` //页面HTML语言
+			Pagelanguagedir      string `json:"pagelanguagedir"`
+			Touched              string `json:"touched"` //创建时间
+			Lastrevid            int    `json:"lastrevid"`
+			Length               int    `json:"length"`
+			FullURL              string `json:"fullurl"`      //页面完整URL
+			EditURL              string `json:"editurl"`      //页面编辑URL
+			CanonicalURL         string `json:"canonicalurl"` //页面规范的URL
+		} `json:"pages"`
+	} `json:"query"`
+}
+
+/**
+ * @description: 查询页面信息，返回带URL
+ * @param {string} WikiLink Wiki连接
+ * @param {string} title 页面标题
+ * @return {*}
+ */
+func QueryInfoUrl(WikiLink string, title string) (QueryInfoUrlJson, error) {
 	url := WikiLink + "/api.php?action=query&prop=info&inprop=url&format=json&titles=" + title
 	body, err := utils.HttpRequest(url)
 
-	info := make(map[string]interface{})
+	var info QueryInfoUrlJson
 	json.Unmarshal([]byte(body), &info)
 	return info, err
 }
